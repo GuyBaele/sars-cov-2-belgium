4736c1ba5443ccc40e251d635ad788d3464b7c07
Barney <barney.potter.24@gmail.com>
2021-01-07 09:26:19 +0100

    Updated build with updated locations and newly ignored strains

---
+++ scripts/renameFiles.sh
@@ -0,0 +1,3 @@
+mv auspice/ncov_belgium_root-sequence.json auspice/sars-cov-2-belgium_root-sequence.json
+mv auspice/ncov_belgium_tip-frequencies.json auspice/sars-cov-2-belgium_tip-frequencies.json
+mv auspice/ncov_belgium.json auspice/sars-cov-2-belgium.json
