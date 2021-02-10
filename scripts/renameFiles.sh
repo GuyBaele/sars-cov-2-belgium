b223ad878e5412fd019b8404d4c26f3be20beacc
Barney <barney.potter.24@gmail.com>
2021-02-10 14:18:05 +0100

    Optimize data ingest for pangolin-based standard builds

--- scripts/renameFiles.sh
+++ scripts/renameFiles.sh
@@ -2,6 +2,6 @@
 # mv auspice/ncov_belgium_tip-frequencies.json auspice/sars-cov-2-belgium_tip-frequencies.json
 # mv auspice/ncov_belgium.json auspice/sars-cov-2-belgium.json
 
-mv auspice/ncov_belgium_root-sequence.json auspice/sars-cov-2-belgium_voc_root-sequence.json
-mv auspice/ncov_belgium_tip-frequencies.json auspice/sars-cov-2-belgium_voc_tip-frequencies.json
-mv auspice/ncov_belgium.json auspice/sars-cov-2-belgium_voc.json
+mv auspice/ncov_belgium-voc_root-sequence.json auspice/sars-cov-2-belgium_voc_root-sequence.json
+mv auspice/ncov_belgium-voc_tip-frequencies.json auspice/sars-cov-2-belgium_voc_tip-frequencies.json
+mv auspice/ncov_belgium-voc.json auspice/sars-cov-2-belgium_voc.json
